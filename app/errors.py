274d44da465a7751306445bbdc3cfacf4e6da74b
matzxcss <matheuslacerda01@proton.me>
2026-01-29 17:32:48 -0300

    solution is coming

---
+++ app/errors.py
@@ -0,0 +1,20 @@
+class VaccineError(Exception):
+    def __init__(self, message: str = "Vaccine error occurred.") -> None:
+        super().__init__(message)
+
+
+class NotVaccinatedError(VaccineError):
+    def __init__(self, message: str = "Visitor is not vaccinated.") -> None:
+        super().__init__(message)
+
+
+class OutdatedVaccineError(VaccineError):
+    def __init__(self, message: str = "Vaccine is expired.") -> None:
+        super().__init__(message)
+
+
+class NotWearingMaskError(Exception):
+    def __init__(
+        self, message: str = "Visitor is not wearing a mask."
+    ) -> None:
+        super().__init__(message)
